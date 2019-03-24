7b32e2a93877308a35197f09af04ec1714b79501
Pawel Siwon <siwonpawel@gmail.com>
2019-03-24 20:55:22 +0100

    Adds test

--- backend/projektzespolowy/src/test/java/psk/projektzespolowy/DictionaryTest.java
+++ backend/projektzespolowy/src/test/java/psk/projektzespolowy/DictionaryTest.java
@@ -1,12 +1,24 @@
 package psk.projektzespolowy;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import psk.dictionary.exception.NodeNotFound;
 import psk.dictionary.model.Dictionary;
 
 public class DictionaryTest {
 	
 	Dictionary dictionary;
+	
+	@Before
+	public void createDictionary() {
+		dictionary = new Dictionary("PL", "EN");
+	}
 
 	@Test
 	public void create() {
@@ -17,12 +29,30 @@ public class DictionaryTest {
 	@Test
 	public void addWord() {
 		
-		dictionary = new Dictionary("PL", "EN");
-		
 		String baseLanguageWord = "kot";
 		String translatedWord = "cat";
 		
 		dictionary.addWord(baseLanguageWord, translatedWord);
+	}
+	
+	@Test
+	public void getWord() {
+		
+		String baseLanguageWord = "kot";
+		String translatedWord = "cat";
+		
+		addWord();
+		
+		List<String> getWord = null;
+		try {
+			getWord = dictionary.getTranslations(baseLanguageWord);
+		} catch (NodeNotFound e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+		assertTrue(getWord.contains(translatedWord));
 	}
     
 }
