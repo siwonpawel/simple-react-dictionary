49926afe98198675d748448c72c04ac8628dd4f8
Kamil Kołodziejczyk <kolodziejczykamil@gmail.com>
2019-04-07 14:04:22 +0200

    hotfix

--- frontend/src/components/wordForm.jsx
+++ frontend/src/components/wordForm.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import { Form, Input, Button } from "antd";
 
 class WordForm extends Component {
-  state = {};
+  state = {
+    plword: "",
+    enword: ""
+  };
+  handleSubmit = () => {
+    console.log(this.state.plword, this.state.enword);
+  };
   render() {
-    return <h1>Add Word</h1>;
+    return (
+      <Form className="add-word-form">
+        <Form.Item>
+          <Input
+            value={this.state.plword}
+            onChange={e => this.setState({ plword: e.target.value })}
+            placeholder="Słowo w języku polskim"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            value={this.state.enword}
+            onChange={e => this.setState({ enword: e.target.value })}
+            placeholder="Tłumaczenie w języku angielskim"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button onClick={this.handleSubmit} type="primary">
+            Dodaj słowo
+          </Button>
+        </Form.Item>
+      </Form>
+    );
   }
 }
 
