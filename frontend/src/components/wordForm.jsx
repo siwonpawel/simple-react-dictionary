db521d23b39f891ffbbc1049867b7799b38f7047
Kamil Kołodziejczyk <kolodziejczykamil@gmail.com>
2019-04-07 14:34:19 +0200

    create form to add a new word in dictionary

--- frontend/src/components/wordForm.jsx
+++ frontend/src/components/wordForm.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
+import { Form, Input, Button } from "antd";
+import axios from "axios";
 
 class WordForm extends Component {
-  state = {};
+  state = {
+    enword: "",
+    plword: ""
+  };
+  handleSubmit = () => {
+    const { enword, plword } = this.state;
+    const apiUrl = "localhost:8080/api/dictionary/pl/en" + plword;
+    axios
+      .post(apiUrl, {
+        translations: [enword]
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+
+    this.setState({ enword: "", plword: "" });
+  };
   render() {
-    return <h1>Add Word</h1>;
+    return (
+      <Form className="add-word-form">
+        <h1>Dodaj tłumaczenie do słownika</h1>
+        <Form.Item>
+          <Input
+            value={this.state.plword}
+            onChange={e => this.setState({ plword: e.target.value })}
+            placeholder="Słowo w języku polskim"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            value={this.state.enword}
+            onChange={e => this.setState({ enword: e.target.value })}
+            placeholder="Tłumaczenie w języku angielskim"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" onClick={this.handleSubmit}>
+            Dodaj tłumaczenie
+          </Button>
+        </Form.Item>
+      </Form>
+    );
   }
 }
 
